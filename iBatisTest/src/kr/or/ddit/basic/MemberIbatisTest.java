9b55c8aa80caafdbb0c663f0299db8400fb6696c
ChoiYoonSung <48468209+ChoiYoonSung@users.noreply.github.com>
2021-03-17 21:58:57 +0900

    HighJava_0317

--- iBatisTest/src/kr/or/ddit/basic/MemberIbatisTest.java
+++ iBatisTest/src/kr/or/ddit/basic/MemberIbatisTest.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.io.Reader;
 import java.nio.charset.Charset;
 import java.sql.SQLException;
-import java.util.List;
 
 import com.ibatis.common.resources.Resources;
 import com.ibatis.sqlmap.client.SqlMapClient;
