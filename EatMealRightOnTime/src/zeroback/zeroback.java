bd5d1052d85da439317dcbadec47cce4b5f19787
ChoiYoonSung <48468209+ChoiYoonSung@users.noreply.github.com>
2021-04-15 21:50:58 +0900

    HighJava_21.04.15

---
+++ EatMealRightOnTime/src/zeroback/zeroback.java
@@ -0,0 +1,13 @@
+package zeroback;
+
+public class zeroback {
+	String name;
+	int age;
+	
+	
+	public static void main(String[] args) {
+		System.out.println("하이하이하이");
+		System.out.println("testtesttest");
+	}
+	
+}
